92ddca2f1ce0e695b54bce00b7eba23638840127
ratanawnk <sanratana18@gmail.com>
2024-10-05 23:01:15 +0700

    update login form

--- src/app/auth/login/components/LoginForm.tsx
+++ src/app/auth/login/components/LoginForm.tsx
@@ -26,11 +26,9 @@ import { useState } from "react";
 import { login } from "@/services/auth.service";
 import { loginSchema } from "@/schema/definition";
 import toast from "react-hot-toast";
-import Toast from "@/modal/toaster";
 import { useRouter } from "next/navigation";
 import { ButtonLoading } from "@/components/ButtonLoading";
 export function LoginForm() {
-  const [errors, setErrors] = useState(null);
   const router = useRouter();
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -60,85 +58,65 @@ export function LoginForm() {
       });
   }
 
-  function handleClearErrors() {
-    setErrors(null);
-  }
-
   return (
-    <>
-      <Toast />
-      <Form {...form}>
-        <form onSubmit={form.handleSubmit(onSubmit)}>
-          <Card className="w-full max-w-sm">
-            <CardHeader>
-              <CardTitle className="text-2xl">Login</CardTitle>
-              <CardDescription>
-                Enter your username and password below to login to your account.
-              </CardDescription>
-            </CardHeader>
-            <CardContent className="grid gap-4">
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="username"
-                  render={({ field }) => (
-                    <FormItem onClick={handleClearErrors}>
-                      <FormLabel>Username</FormLabel>
-                      <FormControl>
-                        <Input
-                          autoFocus
-                          autoComplete="username"
-                          placeholder="username"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              <div className="grid gap-2">
-                <FormField
-                  control={form.control}
-                  name="password"
-                  render={({ field }) => (
-                    <FormItem onClick={handleClearErrors}>
-                      <FormLabel>Password</FormLabel>
-                      <FormControl>
-                        <Input
-                          type="password"
-                          autoComplete="current-password"
-                          placeholder="*********"
-                          {...field}
-                        />
-                      </FormControl>
-                      <FormMessage />
-                    </FormItem>
-                  )}
-                />
-              </div>
-              {errors != null ? (
-                <Alert variant="destructive">
-                  <ExclamationTriangleIcon className="h-4 w-4" />
-                  <AlertTitle>Invalid</AlertTitle>
-                  <AlertDescription>{errors}</AlertDescription>
-                </Alert>
-              ) : (
-                ""
-              )}
-            </CardContent>
-            <CardFooter>
-              <ButtonLoading
-                isLoading={loading}
-                className="w-full"
-                type="submit"
-              >
-                Sign in
-              </ButtonLoading>
-            </CardFooter>
-          </Card>
-        </form>
-      </Form>
-    </>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <Card className="w-full max-w-sm">
+          <CardHeader>
+            <CardTitle className="text-2xl">Login</CardTitle>
+            <CardDescription>
+              Enter your username and password below to login to your account.
+            </CardDescription>
+          </CardHeader>
+          <CardContent className="grid gap-4">
+            <div className="grid gap-2">
+              <FormField
+                control={form.control}
+                name="username"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Username</FormLabel>
+                    <FormControl>
+                      <Input
+                        autoFocus
+                        autoComplete="username"
+                        placeholder="username"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+            <div className="grid gap-2">
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="password"
+                        autoComplete="current-password"
+                        placeholder="*********"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+          </CardContent>
+          <CardFooter>
+            <ButtonLoading isLoading={loading} className="w-full" type="submit">
+              Login
+            </ButtonLoading>
+          </CardFooter>
+        </Card>
+      </form>
+    </Form>
   );
 }
