3e40ff183435e5b727b2e3be1dc6e9f8e87b8e03
ratanawnk <sanratana18@gmail.com>
2024-10-09 11:24:17 +0700

    make sidebar smaller

--- src/components/dashboard/Sidebar.tsx
+++ src/components/dashboard/Sidebar.tsx
@@ -75,10 +75,9 @@ const Sidebar = () => {
   return (
     <div className="hidden border-r bg-muted/40 md:block">
       <div className="flex h-full max-h-screen flex-col gap-2">
-        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+        <div className="flex h-14 items-center justify-center border-b px-4 lg:h-[60px] lg:px-6">
           <Link href="/" className="flex items-center gap-2 font-semibold">
-            <Package2 className="h-6 w-6" />
-            <span>Khmer GRS Academy</span>
+            <span className="text-sm">Khmer GRS Academy</span>
           </Link>
         </div>
         <div className="flex-1">
